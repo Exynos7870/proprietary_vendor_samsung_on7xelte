3fb6d51f1a4d69197c22835886e48a202ddfe7f5
DarkLord1731 <siddhantnaik17@gmail.com>
2018-11-13 21:31:14 +0530

    on7xelte: Add prebuilts from N BSP

--- on7xelte-vendor.mk
+++ on7xelte-vendor.mk
@@ -122,5 +122,23 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/on7xelte/proprietary/vendor/lib/vendor.samsung.hardware.radio.channel@1.0_vendor.so:system/vendor/lib/vendor.samsung.hardware.radio.channel@1.0_vendor.so \
     vendor/samsung/on7xelte/proprietary/vendor/lib/vendor.samsung.hardware.radio.secoemhook@1.0_vendor.so:system/vendor/lib/vendor.samsung.hardware.radio.secoemhook@1.0_vendor.so \
     vendor/samsung/on7xelte/proprietary/vendor/lib/vendor.samsung.hardware.radio@1.1_vendor.so:system/vendor/lib/vendor.samsung.hardware.radio@1.1_vendor.so
+    
 # Create Mali links for Vulkan and OpenCL
 PRODUCT_PACKAGES += libGLES_mali
+
+##BSP
+PRODUCT_COPY_FILES += \
+    vendor/samsung/on7xelte/proprietary/lib/hw/hwcomposer.exynos5.so:system/lib/hw/hwcomposer.exynos5.so \
+    vendor/samsung/on7xelte/proprietary/lib/hw/memtrack.exynos5.so:system/lib/hw/memtrack.exynos5.so \
+    vendor/samsung/on7xelte/proprietary/lib/libexynosdisplay.so:system/lib/libexynosdisplay.so \
+    vendor/samsung/on7xelte/proprietary/lib/libfimg.so:system/lib/libfimg.so \
+    vendor/samsung/on7xelte/proprietary/lib/libhdmi.so:system/lib/libhdmi.so \
+    vendor/samsung/on7xelte/proprietary/lib/libhwcutils.so:system/lib/libhwcutils.so \
+    vendor/samsung/on7xelte/proprietary/lib/libion_exynos.so:system/lib/libion_exynos.so \
+    vendor/samsung/on7xelte/proprietary/lib/libmpp.so:system/lib/libmpp.so \
+    vendor/samsung/on7xelte/proprietary/lib/libcsc.so:system/lib/libcsc.so \
+    vendor/samsung/on7xelte/proprietary/lib/libexynosgscaler.so:system/lib/libexynosgscaler.so \
+    vendor/samsung/on7xelte/proprietary/lib/libexynosscaler.so:system/lib/libexynosscaler.so \
+    vendor/samsung/on7xelte/proprietary/lib/libexynosutils.so:system/lib/libexynosutils.so \
+    vendor/samsung/on7xelte/proprietary/lib/libexynosv4l2.so:system/lib/libexynosv4l2.so \
+    vendor/samsung/on7xelte/proprietary/lib/libhwjpeg.so:system/lib/libhwjpeg.so
